e65cc3e21a2432c14393631e3377c75ab30685c2
Will <willwillwill123@gmail.com>
2022-01-25 10:38:14 -0800

    all files committed for the first time

---
+++ stat_prac/static/main.js
@@ -0,0 +1,12 @@
+function buttonFunc() {
+    document.body.style.backgroundColor = 'green';
+}
+
+function backhome() {
+    document.body.style.backgroundColor = 'blue';
+
+}
+function snuffy() {
+    document.body.style.backgroundColor = 'black';
+
+}
